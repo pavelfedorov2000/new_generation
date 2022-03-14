d3c80f1be6540c4af90d8500e9a0c20e3110d7f2
Pavel Fedorov <p.fedorov@imedia.by>
2022-03-14 18:09:19 +0300

    14.03.22 first commit

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -1,49 +1,56 @@
-new LazyLoad();
-//new WOW().init();
-//app.burger.init();
-//app.slider.init();
-//app.menu.init();
-//app.popup.init();
-//app.validation.init();
+window.addEventListener('DOMContentLoaded', () => {
+  new LazyLoad();
 
+  const burgerMenu = document.querySelector('.burger-menu');
+  const burgerMenuBack = document.querySelector('.burger-menu__back');
+  const burgerMenuLabel = document.querySelector('.burger-menu__label');
+  const burgerOpen = document.querySelector('.promo__btn');
 
-// pageup
-/* document.querySelector('.page-up').addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth",
+  if (burgerOpen) {
+    burgerOpen.addEventListener('click', () => {
+      burgerMenu.classList.add('active');
+      burgerMenuLabel.classList.add('active');
     });
-}); */
+  }
 
+  burgerMenuBack.addEventListener('click', () => {
+    burgerMenu.classList.remove('active');
+    burgerMenuLabel.classList.remove('active');
+  });
 
-//smoooth scroll to section
-document.querySelectorAll('.menu__link').forEach(link => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-        window.scrollTo({
-            top: document.getElementById(link.getAttribute('data-section')).offsetTop,
-            behavior: "smooth"
-        });
-        body.classList.remove('_lock');
-        burgerBtn.classList.remove('burger-btn--active');
-        burgerMenu.classList.remove('burger-menu--active');
-    });
+  new Swiper('.news-slider', {
+    loop: true,
+    //preloadImages: false,
+    //lazy: true,
+    slidesPerView: 2,
+    spaceBetween: 60,
+    speed: 1000,
+    /* autoplay: {
+      delay: 3000,
+    }, */
+    /* pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    }, */
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    /* breakpoints: {
+      576: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 4,
+      }
+    }, */
+    on: {
+      slideChange: function () {
+        new LazyLoad();
+      },
+    },
+  });
 });
-
-// Custom jquery select
-/* $('.select__title').on('click', function () {
-    $(this).parents('.').siblings().find('.select__title').removeClass('select__title--active');
-    $(this).parents('.').siblings().find('.select__content').slideUp('300');
-    $(this).toggleClass('select__title--active');
-    $(this).next().slideToggle('300');
-});
-
-$('.select__option').on('click', function () {
-    $(this).parent().prev().text($(this).text());
-    $(this).parent().prev().removeClass('select__title--active');
-    $(this).parent().slideUp('300');
-}); */
-
 // Custom js select
 /* let selectItem = document.querySelector('.select');
 let selectTitle = selectItem.querySelector('.select__title');
@@ -62,120 +69,4 @@ for (let i = 0; i < selectLabels.length; i++) {
         selectTitle.textContent = e.target.textContent;
         selectItem.setAttribute('data-state', '');
     });
-} */
-
-// js counter +/-
-/* const minusBtn = document.querySelector('.minus-btn');
-const plusBtn = document.querySelector('.plus-btn');
-
-if (minusBtn) {
-    minusBtn.addEventListener('click', function (e) {
-        e.preventDefault();
-        let input = this.nextElementSibling;
-        let value = parseInt(input.value);
-        while (value > 0) {
-            value--;
-            break;
-        }
-        input.value = value;
-    });
-}
-if (plusBtn) {
-    plusBtn.addEventListener('click', function (e) {
-        e.preventDefault();
-        let input = this.previousElementSibling;
-        let value = parseInt(input.value);
-        value++;
-        input.value = value;
-    });
-} */
-
-// ymaps
-/* ymaps.ready(init);
-
-function init() {
-
-    var myMap = new ymaps.Map("map", {
-        center: [55.6938, 37.6001],
-        zoom: 4,
-        controls: ['zoomControl']
-    });
-
-
-    var myGeoObjects = [];
-
-    myGeoObjects[0] = new ymaps.Placemark([55.755819, 37.617644], {
-        clusterCaption: 'Заголовок',
-        //balloonContentBody: 'Текст в балуне',
-    }, {
-        // Необходимо указать данный тип макета.
-        iconLayout: 'default#image',
-        iconImageHref: 'img/icons/pin.svg',
-        // Размеры метки.
-        iconImageSize: [45, 50],
-        // Смещение левого верхнего угла иконки относительно
-        // её «ножки» (точки привязки).
-        iconImageOffset: [-3, -42]
-    });
-
-    myGeoObjects[1] = new ymaps.Placemark([59.939099, 30.315877], {
-        clusterCaption: 'Заголовок',
-        //balloonContentBody: 'Текст в балуне',
-    }, {
-        // Необходимо указать данный тип макета.
-        iconLayout: 'default#image',
-        iconImageHref: 'img/icons/pin.svg',
-        // Размеры метки.
-        iconImageSize: [45, 50],
-        // Смещение левого верхнего угла иконки относительно
-        // её «ножки» (точки привязки).
-        iconImageOffset: [-23, -42]
-    });
-
-    myGeoObjects[2] = new ymaps.Placemark([45.035470, 38.975313], {
-        clusterCaption: 'Заголовок',
-        //balloonContentBody: 'Текст в балуне',
-    }, {
-        // Необходимо указать данный тип макета.
-        iconLayout: 'default#image',
-        iconImageHref: 'img/icons/pin.svg',
-        // Размеры метки.
-        iconImageSize: [45, 50],
-        // Смещение левого верхнего угла иконки относительно
-        // её «ножки» (точки привязки).
-        iconImageOffset: [-16, -42]
-    });
-
-    var clusterIcons = [{
-        href: '/images/pointer.png',
-        size: [31, 40],
-        offset: [0, 0]
-    }];
-
-    var clusterer = new ymaps.Clusterer({
-        clusterDisableClickZoom: false,
-        clusterOpenBalloonOnClick: false,
-        // Устанавливаем стандартный макет балуна кластера "Карусель".
-        clusterBalloonContentLayout: 'cluster#balloonCarousel',
-        // Устанавливаем собственный макет.
-        //clusterBalloonItemContentLayout: customItemContentLayout,
-        // Устанавливаем режим открытия балуна. 
-        // В данном примере балун никогда не будет открываться в режиме панели.
-        clusterBalloonPanelMaxMapArea: 0,
-        // Устанавливаем размеры макета контента балуна (в пикселях).
-        clusterBalloonContentLayoutWidth: 300,
-        clusterBalloonContentLayoutHeight: 200,
-        // Устанавливаем максимальное количество элементов в нижней панели на одной странице
-        clusterBalloonPagerSize: 5
-        // Настройка внешего вида нижней панели.
-        // Режим marker рекомендуется использовать с небольшим количеством элементов.
-        // clusterBalloonPagerType: 'marker',
-        // Можно отключить зацикливание списка при навигации при помощи боковых стрелок.
-        // clusterBalloonCycling: false,
-        // Можно отключить отображение меню навигации.
-        // clusterBalloonPagerVisible: false
-    });
-
-    clusterer.add(myGeoObjects);
-    myMap.geoObjects.add(clusterer);
 } */
